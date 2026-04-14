96801fef44f7075b5f82b346ed26d3ca38d983bc
tranvanngan <ngantran.11072005nd@gmail.com>
2026-04-14 10:42:05 +0700

    Delete Student, Subject, and Grade classes
    Removed Student, Subject, and Grade classes.

--- src/models.py
+++ src/models.py
@@ -1,19 +1,1 @@
-class Student:
-    def __init__(self, id, name, age):
-        self.id = id
-        self.name = name
-        self.age = age
-
-class Subject:
-    def __init__(self, id, name, credits):
-        self.id = id
-        self.name = name
-        self.credits = credits
-
-class Grade:
-    def __init__(self, id, student_id, subject_id, score):
-        self.id = id
-        self.student_id = student_id
-        self.subject_id = subject_id
-        self.score = score
 
